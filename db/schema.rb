2ce15702c96238d4f2b7b61595c74a9adea2709e
Matt Pedersen <mpedersen2054@gmail.com>
2013-12-01 14:39:33 -0600

    created M:M association for posts and categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131201191828) do
+ActiveRecord::Schema.define(version: 20131201200518) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
-    t.text    "content"
-    t.integer "user_id"
-    t.integer "post_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "post_id"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "category_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", force: true do |t|
